e81988b1cb934bc8c1875503e14fcc964bba524d
공수정 <0sujeong@nectarsoft.co.kr>
2022-07-21 17:14:22 +0900

    Initial commit

---
+++ src/programmers/level1/K번째수.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+/**
+ * description    :
+ * packageName    : programmers.level1
+ * fileName       : K번째수
+ * author         : Gong sujeong
+ * date           : 2022-07-21
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-07-21        Gong sujeong       최초 생성
+ */
+public class K번째수 {
+    public static void main(String[] args) {
+        
+    }
+}
